93cf3980ee71f513c08a877bc72292b9e37a0fc4
Shreyas <seekshreyas@gmail.com>
2013-10-11 20:32:23 -0700

    started working on the parser

---
+++ parser.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python
+# -*- coding: UTF-8 -*-
+"""
+Parser
+========
+Parse the txt files for collecting reviews
+
+author = "Shreyas"
+email = "shreyas@ischool.berkeley.edu"
+python_version = "Python 2.7.5 :: Anaconda 1.6.1 (x86_64)"
+"""
+
+
+def main():
+    print "hello world"
+
+
+if __name__ == "__main__":
+    main()
+
