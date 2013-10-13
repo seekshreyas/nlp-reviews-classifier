601a2fbf0a0202652737640948cfe7bf02928fa5
Shreyas <seekshreyas@gmail.com>
2013-10-13 01:51:18 -0700

    feature extraction and aggregation working

--- extractor.py
+++ extractor.py
@@ -15,6 +15,8 @@ python_version = "Python 2.7.5 :: Anaconda 1.6.1 (x86_64)"
 """
 from __future__ import division
 from optparse import OptionParser
+from nltk.tokenize import word_tokenize
+
 import os
 
 #using my parser.py file for getting the input
@@ -40,10 +42,34 @@ def getFiles(dir):
     return datafiles
 
 
-def featureExtractor(inputdata):
-    print inputdata[0]
+def featureAggregator(inputdata):
+    """
+    Given a sentence, call the feature extractor and aggregate the
+    returned features into the same dataset
+    """
+    outputdata = []
+    for inputLine in inputdata:
+        # methods to return values of the features
+        charCount = getCharCount(inputLine[3])
+        wordCount = getWordCount(inputLine[3])
+
+        # aggregate those values into 1 tuple of features
+        features = (charCount, wordCount)
+
+        # append those features
+        inputLineList = list(inputLine)
+        inputLineList.extend(features)
+        outputLineTuple = tuple(inputLineList)
+        outputdata.append(outputLineTuple)
+
+    return outputdata
+
+def getCharCount(sent):
+    return int(len(sent))
 
 
+def getWordCount(sent):
+    return len(word_tokenize(sent))
 
 def main():
     userInput = getInput()
@@ -51,7 +77,9 @@ def main():
 
     parsedata = parser.parseFiles(fileList)
 
-    featdata = featureExtractor(parsedata)
+    featdata = featureAggregator(parsedata)
+
+    print featdata[:10]
 
     # print parsedata[:20]
     # print len(parsedata)
